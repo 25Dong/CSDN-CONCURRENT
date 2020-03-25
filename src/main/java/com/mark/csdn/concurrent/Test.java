44b456f23577b732bab1178c6b6a0bff43c334d6
mark <1685368363@qq.com>
2020-03-25 15:29:57 +0800

    测试第一次提交

---
+++ src/main/java/com/mark/csdn/concurrent/Test.java
@@ -0,0 +1,18 @@
+package com.mark.csdn.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @Description:
+ * @Author: Mark
+ * @CreateDate: 2020/3/25 15:26
+ * @Version: 3.00.001
+ * @Copyright : 豆浆油条个人非正式工作室
+ */
+@Slf4j
+public class Test {
+
+    public static void main(String[] args) {
+        log.info("test");
+    }
+}
